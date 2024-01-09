d3708f2d50bbc9775a55304856746b0352e91905
Dima <uvarov3123@gmail.com>
2024-01-09 16:34:36 +0200

    first commit and add header

---
+++ src/js/files/script.js
@@ -0,0 +1,49 @@
+// Підключення функціоналу "Чертоги Фрілансера"
+import { bodyLock, bodyUnlock, isMobile } from "./functions.js"
+// Підключення списку активних модулів
+import { flsModules } from "./modules.js"
+
+window.addEventListener("load", pageLoad)
+
+function pageLoad() {
+  const htmlTag = document.documentElement
+
+  document.addEventListener("click", e => {
+    const targetElement = e.target
+
+    let catalog = document.querySelector("[data-catalog]")
+
+    if (catalog && targetElement.closest("[data-catalog-open]")) {
+      bodyLock()
+      htmlTag.classList.toggle("open-catalog")
+    }
+    // || htmlTag.closest('.open-catalog') && !targetElement.closest(".catalog-header__body") && !targetElement.closest('[data-catalog-open]')
+    if (
+      htmlTag.closest(".open-catalog") &&
+      !targetElement.closest(".catalog-header__body") &&
+      !targetElement.closest("[data-catalog-open]")
+    ) {
+      htmlTag.classList.toggle("open-catalog")
+      bodyUnlock()
+    }
+
+    /////
+
+    if (targetElement.closest("[data-cart]")) {
+      bodyLock()
+      htmlTag.classList.add("open-cart")
+    }
+
+    if (
+      (htmlTag.closest(".open-cart") && !targetElement.closest(".cart") && !targetElement.closest("[data-cart]")) ||
+      targetElement.closest("[data-cart-close]")
+    ) {
+      htmlTag.classList.remove("open-cart", "open-cart-form")
+      bodyUnlock()
+    }
+
+    if (htmlTag.closest(".open-cart") && targetElement.closest("[data-cart-form]")) {
+      htmlTag.classList.add("open-cart-form")
+    }
+  })
+}
